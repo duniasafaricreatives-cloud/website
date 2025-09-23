94334e059255d772f755f872b4a40f5cb8c15ac3
duniasafaricreatives-cloud <duniasafaricreatives@gmail.com>
2025-09-23 20:22:12 +0000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,46 @@
-import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Packages from './components/Packages';
-import GroupPerks from './components/GroupPerks';
-import Testimonials from './components/Testimonials';
-import FAQ from './components/FAQ';
-import Newsletter from './components/Newsletter';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Packages from "./components/Packages";
+import GroupPerks from "./components/GroupPerks";
+import Testimonials from "./components/Testimonials";
+import FAQ from "./components/FAQ";
+import Newsletter from "./components/Newsletter";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Blog from "./components/Blog";
+import BlogPage from "./pages/BlogPage"; // ✅ New file for full blog
 
 function App() {
   return (
     <div className="min-h-screen">
       <Header />
       <main>
-        <Hero />
-        <About />
-        <Packages />
-        <GroupPerks />
-        <Testimonials />
-        <FAQ />
-        <Blog />
-        <Newsletter />
-        <Contact />
+        <Routes>
+          {/* Landing Page */}
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <About />
+                <Packages />
+                <GroupPerks />
+                <Testimonials />
+                <FAQ />
+                <Blog /> {/* ✅ Shows blog preview */}
+                <Newsletter />
+                <Contact />
+              </>
+            }
+          />
+
+          {/* Full Blog Page */}
+          <Route path="/blog" element={<BlogPage />} />
+        </Routes>
       </main>
       <Footer />
     </div>

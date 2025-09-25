d3ad6ca74f14cdf9586f8e2fd74766cf404ed291
duniasafaricreatives-cloud <duniasafaricreatives@gmail.com>
2025-09-25 22:22:32 +0000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Footer from "./components/Footer";
 
 import BlogListingPage from "./pages/BlogListingPage"; // ✅ new
 import BlogPostPage from "./pages/BlogPostPage";       // ✅ new
+import AboutFounderPage from "./pages/AboutFounderPage";
 
 function App() {
   const location = useLocation();

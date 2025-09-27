5ab4acf34b53a6bd199c3c9e6f9e1cfc42eec2da
duniasafaricreatives-cloud <duniasafaricreatives@gmail.com>
2025-09-27 11:41:44 +0000

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
       id: 1,
       title: "AFCON 2025: Which Team Are You Supporting?",
       subtitle: "Your Morocco Adventure Awaits",
-      image: "https://www.moroccoworldnews.com/wp-content/uploads/2025/07/Prince-Moulay-Abdellah-Stadium.webp" // stadium crowd
+      image: "https://images.unsplash.com/photo-1587368062478-e804f5e2a55a?q=80&w=1023&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpeg" // stadium crowd
     },
     {
       id: 2,

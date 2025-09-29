f106a79079c4e737d66a03a9b3f9c3663ae97d82
duniasafaricreatives-cloud <duniasafaricreatives@gmail.com>
2025-09-29 11:40:55 +0000

    Updated AboutFounderPage.tsx

--- src/pages/AboutFounderPage.tsx
+++ src/pages/AboutFounderPage.tsx
@@ -1,6 +1,26 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay, EffectFade } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
 
 const AboutFounderPage = () => {
+  const travelImages = [
+    { src: "/Akchour waterfalls.jpg", caption: "Akchour Waterfalls" },
+    { src: "/Chefchaoun.jpg", caption: "Chefchaoun" },
+    { src: "/Meknes.jpg", caption: "Meknes" },
+    { src: "/Mr Zalagh_Fes.jpg", caption: "Mr Zalagh, Fes" },
+    { src: "/Mt. Toubal.jpg", caption: "Mt. Toubal" },
+    { src: "/Mt. Toubkal summit.jpg", caption: "Mt. Toubkal Summit" },
+    { src: "/Oujoud waterfalls.jpg", caption: "Oujoud Waterfalls" },
+    { src: "/Ourika.jpg", caption: "Ourika" },
+    { src: "/Tinghir gorge.jpg", caption: "Tinghir Gorge" },
+    { src: "/Ziz valley.jpg", caption: "Ziz Valley" },
+  ];
+
   return (
     <main className="py-20 bg-gray-50">
       <div className="max-w-4xl mx-auto px-6">
@@ -62,6 +82,43 @@ const AboutFounderPage = () => {
             tours, it’s about creating lifelong memories and building connections
             between cultures.
           </p>
+        </section>
+
+        {/* Travel Memories Slider */}
+        <section className="mb-12">
+          <h2 className="text-2xl font-semibold mb-6 text-center text-burgundy-900">
+            Travel Memories
+          </h2>
+          <Swiper
+            modules={[Navigation, Pagination, Autoplay, EffectFade]}
+            effect="fade"
+            fadeEffect={{ crossFade: true }}
+            spaceBetween={20}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            autoplay={{ delay: 3000, disableOnInteraction: false }}
+            breakpoints={{
+              768: { slidesPerView: 2, spaceBetween: 20 },
+              1024: { slidesPerView: 3, spaceBetween: 30 },
+            }}
+            className="rounded-2xl shadow-lg"
+          >
+            {travelImages.map((img, index) => (
+              <SwiperSlide key={index}>
+                <div className="relative">
+                  <img
+                    src={img.src}
+                    alt={img.caption}
+                    className="w-full h-80 object-cover rounded-2xl"
+                  />
+                  <p className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-black/60 text-white px-4 py-1 rounded-full text-sm">
+                    {img.caption}
+                  </p>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </section>
       </div>
     </main>

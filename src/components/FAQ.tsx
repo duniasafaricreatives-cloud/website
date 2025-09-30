eaa4d731034cfcf4d6c149ead57af81a580e71c1
duniasafaricreatives-cloud <duniasafaricreatives@gmail.com>
2025-09-30 16:05:17 +0000

    Updated FAQ.tsx

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -7,7 +7,7 @@ const FAQ = () => {
   const faqs = [
     {
       question: 'What games do the AFCON packages cover?',
-      answer: 'Our packages include 2 games in Fes or Marrakech for Nigerian and Ivorian fans respectively, blending football with culture and exploration. Want 3 games plus New Year in Morocco? Contact our travel agent for the extended itinerary. We also have a 3 city-3 game package (Casa, Fes, Marrakech circuit)'
+      answer: 'Our packages include 2 games in Fes, Rabat & Marrakech for Nigerian, Tanzanian and Ivorian fans respectively, blending football with culture and exploration. Want 3 games plus New Year in Morocco? Contact our travel agent for the extended itinerary.'
     },
     {
       question: 'About booking process',

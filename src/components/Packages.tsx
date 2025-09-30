a1af243491b9fbd3283b11903f9ad7228c6d2b9f
ketepacoast-ops <ketepacoast@gmail.com>
2025-09-30 21:01:41 +0000

    Updated Packages.tsx

--- src/components/Packages.tsx
+++ src/components/Packages.tsx
@@ -61,7 +61,7 @@ const Packages = () => {
             Choose your perfect Morocco AFCON experience
           </p>
           <p className="text-xl text-gray-300 max-w-3xl mx-auto">
-            Packages expire on 31st October
+            Offer ends 31st October
           </p>
         </div>
 

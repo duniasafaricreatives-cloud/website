bc0431d9b697d8183ebc984dd8a8d7acd9f51bac
ketepacoast-ops <ketepacoast@gmail.com>
2025-09-30 21:07:30 +0000

    Updated ItineraryStars.tsx

--- src/pages/ItineraryStars.tsx
+++ src/pages/ItineraryStars.tsx
@@ -83,7 +83,7 @@ const ItineraryStars = () => {
         <table className="w-full border-collapse shadow-lg rounded-lg overflow-hidden">
           <thead>
             <tr>
-              <th className="bg-gray-100 p-4 text-left">Features</th>
+              <th className="bg-gray-100 p-4 text-left">7 Days, 6 Nights (2 Games Itinerary) Features</th>
               {packages.map((pkg) => (
                 <th
                   key={pkg.name}

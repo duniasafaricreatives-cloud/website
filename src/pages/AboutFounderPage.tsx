cc4470e7264d5507bcc4c25584ba7fc9b4af0a19
duniasafaricreatives-cloud <duniasafaricreatives@gmail.com>
2025-10-02 02:17:04 +0000

    Updated AboutFounderPage.tsx

--- src/pages/AboutFounderPage.tsx
+++ src/pages/AboutFounderPage.tsx
@@ -101,7 +101,7 @@ const AboutFounderPage = () => {
       <div className="max-w-4xl mx-auto px-6">
         {/* Page Title */}
         <h1 className="text-4xl font-bold mb-12 text-center text-burgundy-900">
-          {t('aboutFounderPage.title')}
+          {t('About Founder ')}
         </h1>
         <div className="w-24 h-1 bg-amber-600 mx-auto mb-6"></div>
 

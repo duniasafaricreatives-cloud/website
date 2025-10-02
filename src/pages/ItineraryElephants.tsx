aeb51297d6d531ccc4faa681d5995e80b7990ddf
ketepacoast-ops <ketepacoast@gmail.com>
2025-10-02 14:01:09 +0000

    Updated ItineraryElephants.tsx

--- src/pages/ItineraryElephants.tsx
+++ src/pages/ItineraryElephants.tsx
@@ -227,7 +227,7 @@ const ItineraryElephants = () => {
                   rel="noopener noreferrer"
                   className="block w-full mt-4 bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 text-center"
                 >
-                  {t('common.reserveNow')} EN
+                  {t('common.reserveNow')} FN
                 </a>
               </div>
             )}
